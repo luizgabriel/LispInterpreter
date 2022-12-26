1748d5aa502ca837d76aaae915953677a2debc1f
Luiz Gabriel <luizgabriel.info@gmail.com>
2022-12-26 17:19:03 -0300

    Add basic compiler support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+mod parsing;
+mod evaluation;
+
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
+
 fn main() {
-    println!("Hello, world!");
+    // Set up the rustyline editor to handle input and output
+    let mut rl = Editor::<()>::new().expect("Could not initialize rustyline::Editor.");
+
+    // Load any previously saved history
+    rl.load_history("history.txt").unwrap_or_default();
+
+    loop {
+        // Read a line of input from the user
+        let readline = rl.readline("> ");
+        let input = match readline {
+            Ok(line) => line,
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        };
+
+        // Save the input to history
+        rl.add_history_entry(input.as_str());
+
+        // Parse the input
+        let expr = match parsing::parse(&input) {
+            Ok((_, expr)) => expr,
+            Err(e) => {
+                println!("Error: {}", e);
+                continue;
+            }
+        };
+
+        // Evaluate the expression
+        let result = match evaluation::eval(&expr) {
+            Ok(result) => result,
+            Err(e) => {
+                println!("Error: {}", e);
+                continue;
+            }
+        };
+
+        println!("{}", result);
+    }
+
+    // Save the history to a file
+    rl.save_history("history.txt").unwrap();
 }
